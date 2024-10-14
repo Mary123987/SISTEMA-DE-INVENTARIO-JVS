3ae5f79226eef70dabb9e18fefd55b4f30f1cda1
Mary Rojas <maryrojascordova08@gmail.com>
2024-10-13 22:14:09 -0500

    Archivos iniciales

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SISTEMA_DE_INVENTARIO_JVS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
