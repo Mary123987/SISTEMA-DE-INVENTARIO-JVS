900376c1ec4f7323ae35cffc6fbd4f1abf3dcec5
Carlos Chavez <carloschave_2003_07@hotmail.com>
2024-10-15 11:47:15 -0500

    CambiosV2

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SISTEMA_DE_INVENTARIO_JVS.Models;
 
 namespace SISTEMA_DE_INVENTARIO_JVS.Data;
 
@@ -11,4 +12,5 @@ public class ApplicationDbContext : IdentityDbContext
     }
     public DbSet<SISTEMA_DE_INVENTARIO_JVS.Models.Producto> DataProducto { get; set; }
     public DbSet<SISTEMA_DE_INVENTARIO_JVS.Models.Proveedor> DataProveedor { get; set; }
+    public DbSet<Usuario> DataUsuario { get; set; }
 }
